f151d66070a0cfa96a09f39ff46310a3e4dc6be8
Carl Schultz <cschultz162@gmail.com>
2025-06-22 18:41:51 -0500

    testing diskAPI

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -4,4 +4,7 @@ registerSchedule(0, 200, {
         handler: function() {
             var world = Server.getWorld("world");    
             Server.broadcastMessage("Gametime: " + world.getTime());
+            DiskAPI.loadFile("test", false, true);
+            DiskAPI.setVar("test", "gametime", world.getTime(), true);
+            DiskAPI.saveFile("test",false,true);
 }}, "handler");
