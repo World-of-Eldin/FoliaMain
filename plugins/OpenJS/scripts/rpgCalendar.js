23a8a341590573898e609a6dc50fd446882b9380
Carl Schultz <cschultz162@gmail.com>
2025-06-22 18:43:36 -0500

    spelled DiskApi wrong

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -4,7 +4,7 @@ registerSchedule(0, 200, {
         handler: function() {
             var world = Server.getWorld("world");    
             Server.broadcastMessage("Gametime: " + world.getTime());
-            DiskAPI.loadFile("test", false, true);
-            DiskAPI.setVar("test", "gametime", world.getTime(), true);
-            DiskAPI.saveFile("test",false,true);
+            DiskApi.loadFile("test", false, true);
+            DiskApi.setVar("test", "gametime", world.getTime(), true);
+            DiskApi.saveFile("test",false,true);
 }}, "handler");
