77f547075b70c83f9b2866b38c2956dfc71a434d
Carl Schultz <cschultz162@gmail.com>
2025-06-22 18:46:50 -0500

    trying different file extension

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -4,7 +4,7 @@ registerSchedule(0, 200, {
         handler: function() {
             var world = Server.getWorld("world");    
             Server.broadcastMessage("Gametime: " + world.getTime());
-            DiskApi.loadFile("test", false, true);
-            DiskApi.setVar("test", "gametime", world.getTime(), true);
-            DiskApi.saveFile("test",false,true);
+            DiskApi.loadFile("test.yml", true, true);
+            DiskApi.setVar("test.yml", "gametime", world.getTime(), true);
+            DiskApi.saveFile("test.yml",true,true);
 }}, "handler");
