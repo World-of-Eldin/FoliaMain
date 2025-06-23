3f7765fe543710d0656744bfbf266275d09f018c
Brendan280 <brendan.bugeja@outlook.com>
2025-06-23 17:22:11 +1000

    Modified XP script to allow for full inventories

--- plugins/OpenJS/scripts/xpBottles.js
+++ plugins/OpenJS/scripts/xpBottles.js
@@ -3,16 +3,32 @@ registerEvent("org.bukkit.event.player.PlayerInteractEvent", {
         var ItemStack = org.bukkit.inventory.ItemStack; //The Bukkit ItemStack class
         var Material = org.bukkit.Material; //The Bukkit Material class
         var player = event.getPlayer();
+        var inventory = player.getInventory();
         if(event.getClickedBlock() != null && player.hasPermission("eldin.xp.bottle")) {
             if(event.getClickedBlock().type.toString() === "ENCHANTING_TABLE" && event.getAction().toString() === "RIGHT_CLICK_BLOCK") { //Check that block is enchanting table and right click is used
-                if(player.isSneaking() && player.getInventory().getItemInMainHand().type.toString() === "GLASS_BOTTLE") { //Check that player is crouching and holding a glass bottle
+                if(player.isSneaking() && inventory.getItemInMainHand().type.toString() === "GLASS_BOTTLE") { //Check that player is crouching and holding a glass bottle
                     if(event.getHand().toString() === "HAND" && player.getLevel() > 0) { //Ensure that the logic does not run twice because of the off hand and ensure that the player is at least level 1
-                        var material = Material.getMaterial("EXPERIENCE_BOTTLE");
-                        var XPBottle = new ItemStack(material, 1); //Add the exp bottle to player inventory
-                        player.getInventory().addItem(XPBottle);
-                        material = Material.getMaterial("GLASS_BOTTLE")
+                        var material = Material.getMaterial("GLASS_BOTTLE")
                         var glassBottle = new ItemStack(material, 1)
-                        player.getInventory().removeItem(glassBottle); //Remove the glass bottle from player inventory
+                        material = Material.getMaterial("EXPERIENCE_BOTTLE");
+                        var XPBottle = new ItemStack(material, 1); //Add the exp bottle to player inventory
+                        var partialStack = false;
+                        for(var i = 0; i < inventory.getSize(); i++) {
+                            var item = inventory.getItem(i);
+                            if(item != null && item.getType() == material && item.getAmount() < 64) {
+                                partialStack = true;
+                                break;
+                            }
+                        }
+
+                        if(inventory.firstEmpty() >= 0 || partialStack) {
+                            var XPBottle = new ItemStack(material, 1); //Add the exp bottle to player inventory
+                            inventory.addItem(XPBottle);
+                        }
+                        else {
+                            player.getWorld().dropItem(player.getLocation(), XPBottle)
+                        }
+                        inventory.removeItem(glassBottle); //Remove the glass bottle from player inventory
                         player.giveExp(-7); //Subtract 7 exp from the player
                     }
                 }
