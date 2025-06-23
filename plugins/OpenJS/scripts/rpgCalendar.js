fe209808a622dcfa658195d0d1192609555e9a8e
Carl Schultz <cschultz162@gmail.com>
2025-06-22 20:45:53 -0500

    better day handling

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -3,12 +3,14 @@ var Server = plugin.getServer();
 registerSchedule(0, 200, {
         handler: function() {
             DiskApi.loadFile("CalendarData", true, true);
+            var fullTime = Server.getWorld("world").getFullTime();
             var historicalDay =  DiskApi.getVar("CalendarData", "DayCount", 0, true)
-            var currentDay = Math.floor(Server.getWorld("world").getFullTime()/24000);
+            var currentDay = Math.floor(fullTime / 24000);
             
             if (currentDay = historicalDay) {
-                return;
-            } else if (currentDay > historicalDay) {
+                DiskApi.setVar("CalendarData", "DayCount", currentDay, true);
+                DiskApi.saveFile("CalendarData",true,true);
+            } else {
                 Server.broadcastMessage("Dawn of day " + currentDay + "!")
                 DiskApi.setVar("CalendarData", "DayCount", currentDay, true);
                 DiskApi.saveFile("CalendarData",true,true);
