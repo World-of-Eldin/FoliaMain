2727212b8183ddea0a6a2e94ed44848abb152c9c
Carl Schultz <cschultz162@gmail.com>
2025-06-22 21:52:42 -0500

    Added days, months, year handling

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -1,18 +1,68 @@
 var Server = plugin.getServer();
+var Days = [
+  "Lunae",
+  "Martis",
+  "Mercurii",
+  "Iovis",
+  "Veneris",
+  "Saturni",
+  "Solis",
+];
+var Months = [
+  "Hammer",
+  "Alturiak",
+  "Ches",
+  "Tarsakh",
+  "Mirtul",
+  "Kythorn",
+  "Flamerule",
+  "Elesias",
+  "Eleint",
+  "Marpenoth",
+  "Uktar",
+  "Nightal",
+];
 
-registerSchedule(0, 200, {
-        handler: function() {
-            DiskApi.loadFile("CalendarData", true, true);
-            var fullTime = Server.getWorld("world").getFullTime();
-            var historicalDay =  DiskApi.getVar("CalendarData", "DayCount", 0, true)
-            var currentDay = Math.floor(fullTime / 24000);
-            
-            if (currentDay = historicalDay) {
-                DiskApi.setVar("CalendarData", "DayCount", currentDay, true);
-                DiskApi.saveFile("CalendarData",true,true);
-            } else {
-                Server.broadcastMessage("Dawn of day " + currentDay + "!")
-                DiskApi.setVar("CalendarData", "DayCount", currentDay, true);
-                DiskApi.saveFile("CalendarData",true,true);
-            }
-}}, "handler");
+registerSchedule(
+  0,
+  200,
+  {
+    handler: function () {
+      DiskApi.loadFile("CalendarData", false, true);
+      var fullTime = Server.getWorld("world").getFullTime();
+      var historicalDay = parseInt(
+        DiskApi.getVar("CalendarData", "DayCount", 0, true)
+      );
+      var currentDay = parseInt(Math.floor(fullTime / 24000));
+
+      if (currentDay === historicalDay) {
+        return;
+      } else {
+        var dayOfWeek = currentDay % 7;
+        var dayOfMonth = (currentDay % 30) + 1;
+        var monthIndex = Math.floor(currentDay / 30) % 12;
+        var years = Math.floor(currentDay / 360);
+        var yearsMessage = " years of the age of Ruins have passed";
+
+        if (years === 1) {
+          yearsMessage = " year of the age of Ruins has passed";
+        }
+
+        var message =
+          "Today is " +
+          Days[dayOfWeek] +
+          ", " +
+          Months[monthIndex] +
+          " " +
+          dayOfMonth +
+          ", " +
+          years +
+          yearsMessage;
+        Server.broadcastMessage(message);
+        DiskApi.setVar("CalendarData", "DayCount", currentDay, true);
+        DiskApi.saveFile("CalendarData", false, true);
+      }
+    },
+  },
+  "handler"
+);
