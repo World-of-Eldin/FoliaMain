eadde2d7e309c0d71c717464da84316423cd5046
Carl Schultz <cschultz162@gmail.com>
2025-06-22 20:27:54 -0500

    math floor

--- plugins/OpenJS/scripts/rpgCalendar.js
+++ plugins/OpenJS/scripts/rpgCalendar.js
@@ -3,7 +3,7 @@ var Server = plugin.getServer();
 registerSchedule(0, 200, {
         handler: function() {
             var time = Server.getWorld("world").getFullTime();    
-            Server.broadcastMessage("CurrentDay: " + time % 24000);
+            Server.broadcastMessage("CurrentDay: " + Math.floor(time / 24000));
             DiskApi.loadFile("test.yml", true, true);
             DiskApi.setVar("test.yml", "gametime", time, true);
             DiskApi.saveFile("test.yml",true,true);
