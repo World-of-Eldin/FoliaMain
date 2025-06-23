367e84d775bfd17d43e2a3e21444b83b41417824
Carl Schultz <cschultz162@gmail.com>
2025-06-23 03:37:50 -0500

    better boat fix

--- plugins/OpenJS/scripts/IceRoadPrevention.js
+++ plugins/OpenJS/scripts/IceRoadPrevention.js
@@ -1,13 +1,47 @@
+Server = plugin.getServer();
 registerEvent("org.bukkit.event.vehicle.VehicleMoveEvent", {
   handleEvent: function (event) {
     var vehicle = event.getVehicle();
-    var blockUnder = vehicle.getLocation().getBlock().getRelative(0, -1, 0);
-
     if (vehicle.getName().toString().indexOf("BOAT")) {
-      if (blockUnder.getType().toString().indexOf("ICE") >= 0) {
-        var passengers = vehicle.getPassengers();
-
-        for (i = 0; i < passengers.length; i++) {
+      var passengers = vehicle.getPassengers();
+      for (i = 0; i < passengers.length; i++) {
+        if (
+          passengers[i]
+            .getLocation()
+            .getBlock()
+            .getRelative(0, -0.5, 0)
+            .getType()
+            .toString()
+            .indexOf("ICE") >= 0 ||
+          passengers[i]
+            .getLocation()
+            .getBlock()
+            .getRelative(1, -0.5, 0)
+            .getType()
+            .toString()
+            .indexOf("ICE") >= 0 ||
+          passengers[i]
+            .getLocation()
+            .getBlock()
+            .getRelative(-1, -0.5, 0)
+            .getType()
+            .toString()
+            .indexOf("ICE") >= 0 ||
+          passengers[i]
+            .getLocation()
+            .getBlock()
+            .getRelative(0, -0.5, 1)
+            .getType()
+            .toString()
+            .indexOf("ICE") >= 0 ||
+          passengers[i]
+            .getLocation()
+            .getBlock()
+            .getRelative(0, -0.5, 1)
+            .getType()
+            .toString()
+            .indexOf("ICE") >= 0
+        ) {
           vehicle.removePassenger(passengers[i]);
         }
       }
