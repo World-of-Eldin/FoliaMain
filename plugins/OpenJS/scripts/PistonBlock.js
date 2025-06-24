9feef7c5433f0d4230ead813d78ce86d0e235946
Your Name <you@example.com>
2025-06-24 13:52:12 +1000

    Piston block script optimised with forEach

---
+++ plugins/OpenJS/scripts/PistonBlock.js
@@ -0,0 +1,70 @@
+const crops = ["WHEAT", "NETHER_WART", "POTATOES", "CARROTS", "PUMPKIN", "MELON", "BEETROOTS", "SUGAR_CANE", "BAMBOO", "KELP", "CACTUS", "SWEET_BERRY_BUSH", "CHORUS_PLANT", "CHORUS_FLOWER", "RED_MUSHROOM", "BROWN_MUSHROOM"];
+registerEvent("org.bukkit.event.block.BlockPistonExtendEvent", {
+    handleEvent: function(event) {
+        const block = event.getBlock();
+        const direction = event.getDirection().toString();
+        switch(direction) {
+            case "SOUTH":
+                for(z = 1; z <= 14; z++) {
+                    for(y = -1; y <= 2; y++) {
+                        crops.forEach(crop => {
+                            if(block.getRelative(0, y, z).getType().toString() === crop) {
+                                event.setCancelled(true);
+                            }
+                        })
+                    }
+                }
+            
+            case "NORTH":
+            for(z = 1; z >= -14; z--) {
+                for(y = -1; y <= 2; y++) {
+                    crops.forEach(crop => {
+                        if(block.getRelative(0, y, z).getType().toString() === crop) {
+                            event.setCancelled(true);
+                        }
+                    })
+                }
+            }
+        
+            case "EAST":
+            for(x = 1; x <= 14; x++) {
+                for(y = -1; y <= 2; y++) {
+                    crops.forEach(crop => {
+                        if(block.getRelative(x, y, 0).getType().toString() === crop) {
+                            event.setCancelled(true);
+                        }
+                    })
+                }
+            }
+        
+            case "WEST":
+            for(x = 1; x >= -14; x--) {
+                for(y = -1; y <= 2; y++) {
+                    crops.forEach(crop => {
+                        if(block.getRelative(x, y, 0).getType().toString() === crop) {
+                            event.setCancelled(true);
+                        }
+                    })
+                }
+            }
+        
+            case "UP":
+                for(y = 1; y <= 14; y++) {
+                    crops.forEach(crop => {
+                        if(block.getRelative(0, y, z).getType().toString() === crop) {
+                            event.setCancelled(true);
+                        }
+                    })
+                }
+            
+            case "DOWN":
+                for(y = -14; y <= 0; y++) {
+                    crops.forEach(crop => {
+                        if(block.getRelative(0, y, z).getType().toString() === crop) {
+                            event.setCancelled(true);
+                        }
+                    })
+                }
+        }
+    }
+})
