c81f658e5733d52376c6348fb4ab5c82d9e60f2a
Carl Schultz <cschultz162@gmail.com>
2025-10-25 11:42:27 -0500

    armorer shop and script updates

--- plugins/OpenJS/scripts/dropRemove.js
+++ plugins/OpenJS/scripts/dropRemove.js
@@ -1,23 +1,29 @@
-const bannedDrops = ["IRON_INGOT", "GOLD_NUGGET", "GOLD_INGOT", "EMERALD", "COPPER INGOT"]
+const bannedDrops = ["IRON_INGOT", "GOLD_NUGGET", "GOLD_INGOT", "EMERALD", "COPPER_INGOT"]
 
-registerEvent("org.bukkit.event.entity.PlayerDeathEvent", {
+// Prevent any entity (except players) from dropping these items
+registerEvent("org.bukkit.event.entity.EntityDeathEvent", {
     handleEvent: function(event) {
-        const victim = event.getEntity();
-        if(victim.getName()) {
-            let droppedItems = event.getDrops();
-            let itemsToRemove = []; //Iterate through list of dropped items, searching for a banned item
-            droppedItems.forEach(item => {
+        const entity = event.getEntity();
 
-                bannedDrops.forEach(drop => { //Search the array for matches to a banned item
-                    if (item.getType().toString() === drop) {
-                        itemsToRemove.push(item);  //Push the item to the itemsToRemove array
-                    }
-                })
-            });
-            //Remove the items in the itemsToRemove array
-            itemsToRemove.forEach(item => {
-                droppedItems.remove(item);
-            });
+        // Skip if the entity is a player (allow players to drop these items)
+        if (entity.getType().toString() === "PLAYER") {
+            return;
         }
+
+        let droppedItems = event.getDrops();
+        let itemsToRemove = []; // Iterate through list of dropped items, searching for a banned item
+
+        droppedItems.forEach(item => {
+            bannedDrops.forEach(drop => { // Search the array for matches to a banned item
+                if (item.getType().toString() === drop) {
+                    itemsToRemove.push(item); // Push the item to the itemsToRemove array
+                }
+            })
+        });
+
+        // Remove the items in the itemsToRemove array
+        itemsToRemove.forEach(item => {
+            droppedItems.remove(item);
+        });
     }
 })
