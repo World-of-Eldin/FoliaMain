365d48568de1b1771ef190672a2c95eb76e49534
Your Name <you@example.com>
2025-11-27 13:24:15 +1100

    Added Discord logging to ore notification

--- plugins/OpenJS/scripts/OreNotification.js
+++ plugins/OpenJS/scripts/OreNotification.js
@@ -1,9 +1,12 @@
+const Bukkit = org.bukkit.Bukkit;
+const Scheduler = Bukkit.getGlobalRegionScheduler();
+const Console = Bukkit.getConsoleSender();
 let checkedCoords = []; //An array of coordinates where ores have already been mined
 registerEvent("org.bukkit.event.block.BlockBreakEvent", {
     handleEvent: function(event) {
         const blockMined = event.getBlock(); //The block mined by the player
         const bukkit = org.bukkit.Bukkit;
-        const blocks = ["IRON_ORE", "GOLD_ORE", "EMERALD_ORE", "DIAMOND_ORE", "ANCIENT_DEBRIS", "DEEPSLATE_IRON_ORE", "DEEPSLATE_GOLD_ORE", "DEEPSLATE_EMERALD_ORE", "DEEPSLATE_DIAMOND_ORE", "RAW_IRON_BLOCK", "RAW_GOLD_BLOCK", "COPPER_ORE", "DEEPSLATE_COPPER_ORE", "COAL_ORE", "DEEPSLATE_COAL_ORE", "REDSTONE_ORE", "DEEPSLATE_REDSTONE_ORE", "LAPIS_ORE", "DEEPSLATE_LAPIS_ORE"]; //Ores to record
+        const blocks = ["IRON_ORE", "GOLD_ORE", "EMERALD_ORE", "DIAMOND_ORE", "ANCIENT_DEBRIS", "DEEPSLATE_IRON_ORE", "DEEPSLATE_GOLD_ORE", "DEEPSLATE_EMERALD_ORE", "DEEPSLATE_DIAMOND_ORE", "RAW_IRON_BLOCK", "RAW_GOLD_BLOCK", "COPPER_ORE", "COPPER_BLOCK", "DEEPSLATE_COPPER_ORE", "COAL_ORE", "DEEPSLATE_COAL_ORE", "REDSTONE_ORE", "DEEPSLATE_REDSTONE_ORE", "LAPIS_ORE", "DEEPSLATE_LAPIS_ORE"]; //Ores to record
         blocks.forEach(block => {
             if(blockMined.getType().toString() === block) { //Check that the block mined is an ore
                 let oreNotAlreadyDisplayed = true;
@@ -43,11 +46,16 @@ registerEvent("org.bukkit.event.block.BlockBreakEvent", {
                         block = "lapis"
                     }
                     const onlinePlayer = bukkit.getOnlinePlayers();
+                    message = event.getPlayer().name + " found " + oreCount + " " + block + " at " + blockMined.x + ", " + blockMined.y + ", " + blockMined.z
                     onlinePlayer.forEach(player => {
                         if(player.hasPermission("eldin.staff")) { //Check that player is staff
-                            player.sendMessage(event.getPlayer().name + " found " + oreCount + " " + block + " at " + blockMined.x + ", " + blockMined.y + ", " + blockMined.z); //Send the ore found message.
+                            player.sendMessage(message); //Send the ore found message.
                         }
-                    })   
+                    })
+
+                    Scheduler.run(Bukkit.getPluginManager().getPlugin("OpenJS"), function () {
+                        Bukkit.dispatchCommand(Console, "discordsrv bcast #1443427143660077157 " + message); //Send the discord message
+                    });
                 }
             }
         })
